0860040e598856993535ca28582068ca927eef33
Christopher Gosala <cgosala@deloitte.com>
2017-04-07 17:35:43 +0530

    Update main.js
    added log in front end

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
         url:'/upload',
         dataType: 'json',
         done: function(e, data) {
+            console.log(data.result.files);
             $.each(data.result.files, function(index, file) {
                 $('<p/>').text(file.name).appendTo(document.body);
             });
