026df19aeeec92b0f7471e75ccd0a6d3a10904f5
Christopher Gosala <cgosala@deloitte.com>
2017-04-07 17:30:19 +0530

    Update index.js
    returning proper response

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var async = require('async');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,29 +19,6 @@ var jfum = new JFUM({
 });
 
 
-
-// router.post('/upload', function(req, res) {
-//
-//     console.log(req);
-//
-//     // req.file('file')
-//     //     .upload({
-//     //         // ...options here...
-//     //     }, function whenDone(err, uploadedFiles) {
-//     //         if (err) return res.negotiate(err);
-//     //         else return res.ok({
-//     //             files: uploadedFiles,
-//     //             textParams: req.params.all()
-//     //         });
-//     //     });
-//     // console.log('upload Url');
-//     // console.log(req.files)
-//     res.status(200).jsonp({
-//         'message': 'dummy'
-//     });
-// });
-//
-//
 router.options('/upload', jfum.optionsHandler.bind(jfum));
 
 router.post('/upload', jfum.postHandler.bind(jfum), function(req, res) {
@@ -51,31 +30,52 @@ router.post('/upload', jfum.postHandler.bind(jfum), function(req, res) {
             'error': req.jfum.error
         });
     } else {
-        // Here are the uploaded files
-        console.log();
-        for (var i = 0; i < req.jfum.files.length; i++) {
-            var file = req.jfum.files[i];
+        async.each(req.jfum.files, function(file, callback) {
+
+            // Perform operation on file here.
+            console.log('Processing file ' + file);
 
             // Check if file has errors
             if (file.errors.length > 0) {
-                for (var j = 0; i < file.errors.length; i++) {
+                for (var j = 0; j < file.errors.length; j++) {
                     file.errors[j].code
                     file.errors[j].message
                 }
+                callback(file);
 
             } else {
-                console.log('File No - ', (i + 1));
-                console.log('field - ', file.field); // - form field name
-                console.log('path - ', file.path); // - full path to file on disk
-                console.log('name - ', file.name); // - original file name
-                console.log('size - ', file.size); // - file size on disk
-                console.log('mime - ', file.mime); // - file mime type
-                console.log('\n\n');
+
+                var encoding = '';
+
+                fs.readFile(file.path, function(err, data) {
+                    if (err) throw err;
+                    file.buffer = data;
+                    console.log('field - ', file.field); // - form field name
+                    console.log('path - ', file.path); // - full path to file on disk
+                    console.log('name - ', file.name); // - original file name
+                    console.log('size - ', file.size); // - file size on disk
+                    console.log('mime - ', file.mime); // - file mime type
+                    console.log('\n\n');
+
+                    callback();
+                    // console.log(data);
+                });
+
             }
-        }
-        res.status(200).jsonp({
-            'result': {
-                'files': req.jfum.files
+        }, function(err) {
+            // if any of the file processing produced an error, err would equal that error
+            if (err) {
+                // One of the iterations produced an error.
+                // All processing will now stop.
+                console.log(req.jfum.error);
+                res.status(400).jsonp({
+                    'AllFilesWithErrors': req.jfum.files,
+                    'error': err
+                });
+            } else {
+                res.status(200).jsonp({
+                    'files': req.jfum.files
+                });
             }
         });
     }
